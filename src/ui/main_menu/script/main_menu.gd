812acb35038307f48d208031b6f224e4b8af28be
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-04 18:10:24 +0800

    changed new game to point to player scene

--- src/ui/main_menu/script/main_menu.gd
+++ src/ui/main_menu/script/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_NewGameButton_pressed():
-	get_tree().change_scene("res://src/ui/main_menu/scene/new_game_scene.tscn")
+	get_tree().change_scene("res://src/scene/player/player_scene.tscn")
 
 func _on_OptionButton_pressed():
 	get_tree().change_scene("res://src/ui/main_menu/scene/option_scene.tscn")
