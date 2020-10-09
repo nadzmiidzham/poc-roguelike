590939535fc50552e2edef8385fc4816e819b0f7
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-10 01:43:24 +0800

    added basic character movements

--- src/actor/player/player.gd
+++ src/actor/player/player.gd
@@ -2,31 +2,80 @@ class_name Player
 extends KinematicBody2D
 
 export (int) var gravity := 20
-export (int) var speed := 200
-export (int) var jump_force := 20
+export (int) var max_speed := 200
+export (int) var max_slide_speed := 100
+export (int) var acceleration := 20
+export (int) var jump_force := 550
 export (float) var wall_slide_gravity_rate := 1.5
 
-onready var is_facing_right := true
+onready var animation := $Animation
+onready var attack_animation := $AttackAnimation
+onready var ground_check := $GroundCheck
+onready var front_check := $FrontCheck
+
 onready var velocity := Vector2.ZERO
+onready var linear_drag := 0.2
+onready var is_facing_right := true
+onready var is_wall_sliding := false
+onready var is_wall_jumping := false
 
 
-# other methods
-func compute_gravity() -> void:
-	velocity.y += gravity
+func _physics_process(delta):
+	velocity.y += gravity # compute gravity
 
-func compute_velocity() -> void:
-	velocity = move_and_slide(velocity, Vector2.UP)
+	# movement
+	if !is_wall_jumping:
+		if Input.is_action_pressed("right"):
+			velocity.x = min(velocity.x + acceleration, max_speed) # velocity cannot be more than max_speed
+			if is_grounded():
+				animation.play("run")
+		elif Input.is_action_pressed("left"):
+			velocity.x = max(velocity.x - acceleration, -max_speed) # velocity cannot be more than -max_speed
+			if is_grounded():
+				animation.play("run")
+		else:
+			velocity.x = lerp(velocity.x, 0, 0.2)
+			if is_grounded():
+				animation.play("idle")
 
-func move() -> void:
-	var right_input = Input.is_action_pressed("move_right")
-	var left_input = Input.is_action_pressed("move_left")
+	# fall & jump
+	if !is_wall_sliding && is_grounded() && Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_force
 
-	velocity.x = (int(right_input) - int(left_input)) * speed
+	if !is_grounded() && velocity.y > 0:
+		animation.play("fall")
+	elif (is_grounded() || is_wall_jumping) && velocity.y < 0:
+		animation.play("jump")
 
-func check_flip() -> void:
-	if !is_facing_right && (velocity.x > 0):
-		is_facing_right = !is_facing_right
-		self.scale.x *= -1
-	if is_facing_right && (velocity.x < 0):
-		is_facing_right = !is_facing_right
-		self.scale.x *= -1
+	# wall slide & wall jump
+	if !is_grounded() && is_touching_wall() && velocity.y > 0:
+		var slide_speed = gravity / 2
+		is_wall_sliding = true
+		velocity.y = min(velocity.y + slide_speed, max_slide_speed)
+		animation.play("wall_slide")
+	if is_wall_sliding && Input.is_action_just_pressed("jump"):
+		var direction = -1 if is_facing_right else 1
+		velocity = Vector2(direction * max_speed, -jump_force)
+		is_wall_sliding = false
+		is_wall_jumping = true
+
+	if !is_touching_wall():
+		is_wall_sliding = false
+	if is_grounded():
+		is_wall_jumping = false
+
+	velocity = move_and_slide(velocity, Vector2.UP) # compute velocity
+
+func _input(event):
+	if (!is_facing_right && event.is_action_pressed("right")) || (is_facing_right && event.is_action_pressed("left")):
+		flip()
+
+func flip():
+	is_facing_right = !is_facing_right
+	scale.x *= -1
+
+func is_grounded():
+	return ground_check.is_colliding()
+
+func is_touching_wall():
+	return front_check.is_colliding()
