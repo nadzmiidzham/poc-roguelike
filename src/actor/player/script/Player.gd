fe47aa2a423fd5adb25a0158e4817ded05461c1f
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-14 00:38:57 +0800

    added basic attack 1

--- src/actor/player/script/Player.gd
+++ src/actor/player/script/Player.gd
@@ -14,12 +14,33 @@ onready var front_check := $FrontCheck
 
 onready var velocity := Vector2.ZERO
 onready var linear_drag := 0.2
-onready var cur_attack_time := 1
-onready var max_attack_time := 3
+onready var cur_basic_attack_time := 1
+onready var max_basic_attack_time := 3
 onready var is_facing_right := true
 onready var is_wall_sliding := false
 onready var is_wall_jumping := false
+onready var is_attacking := false
 
+
+func _process(delta):
+	# run animation
+	if is_attacking:
+		animation.play("attack_basic_1")
+		return
+
+	if is_grounded():
+		if abs(velocity.x) > 0:
+			animation.play("run")
+		else:
+			animation.play("idle")
+
+	if !is_grounded() && (velocity.y > 0):
+		animation.play("fall")
+	elif (is_grounded() || is_wall_jumping) && (velocity.y < 0):
+		animation.play("jump")
+
+	if is_wall_sliding:
+		animation.play("wall_slide")
 
 func _physics_process(delta):
 	velocity.y += gravity # compute gravity
@@ -28,32 +49,20 @@ func _physics_process(delta):
 	if !is_wall_jumping:
 		if Input.is_action_pressed("right"):
 			velocity.x = min(velocity.x + acceleration, max_speed) # velocity cannot be more than max_speed
-			if is_grounded():
-				animation.play("run")
 		elif Input.is_action_pressed("left"):
 			velocity.x = max(velocity.x - acceleration, -max_speed) # velocity cannot be more than -max_speed
-			if is_grounded():
-				animation.play("run")
 		else:
-			velocity.x = lerp(velocity.x, 0, 0.2)
-			if is_grounded():
-				animation.play("idle")
+			velocity.x = 0 # lerp(velocity.x, 0, 0.5)
 
 	# fall & jump
-	if !is_wall_sliding && is_grounded() && Input.is_action_just_pressed("jump"):
-			velocity.y = -jump_force
-
-	if !is_grounded() && velocity.y > 0:
-		animation.play("fall")
-	elif (is_grounded() || is_wall_jumping) && velocity.y < 0:
-		animation.play("jump")
+	if !is_attacking && !is_wall_sliding && is_grounded() && Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
 
 	# wall slide & wall jump
 	if !is_grounded() && is_touching_wall() && velocity.y > 0:
 		var slide_speed = gravity / 2
 		velocity.y = min(velocity.y + slide_speed, max_slide_speed)
 		is_wall_sliding = true
-		animation.play("wall_slide")
 
 	if is_wall_sliding && Input.is_action_just_pressed("jump"):
 		var direction = -1 if is_facing_right else 1
@@ -66,11 +75,22 @@ func _physics_process(delta):
 	if is_grounded():
 		is_wall_jumping = false
 
+	# attack
+	if !is_touching_wall() && Input.is_action_just_pressed("attack_1"):
+		is_attacking = true
+
+	# prevent ground movement while attacking
+	if is_attacking && is_grounded():
+		velocity.x = 0
+
 	velocity = move_and_slide(velocity, Vector2.UP) # compute velocity
 
 func _input(event):
 	if (!is_facing_right && event.is_action_pressed("right")) || (is_facing_right && event.is_action_pressed("left")):
+		if is_attacking:
+			return
 		flip()
+
 
 func flip():
 	is_facing_right = !is_facing_right
@@ -81,3 +101,8 @@ func is_grounded():
 
 func is_touching_wall():
 	return front_check.is_colliding()
+
+func animation_finished():
+	if (!is_facing_right && Input.is_action_pressed("right")) || (is_facing_right && Input.is_action_pressed("left")):
+		flip()
+	is_attacking = false
