d7eb0cfecada8d73d883018bb84c951b7a82c5e9
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-22 17:05:00 +0800

    added effect animation
    - added attack effect animation
    - implemented damage animation for box

--- src/object/box/BoxController.gd
+++ src/object/box/BoxController.gd
@@ -7,6 +7,10 @@ export(int) var max_hp := 3
 onready var service := $Service
 onready var animated_sprite := $AnimatedSprite
 onready var collision := $CollisionShape2D
+onready var hit_timer := $HitTimer
+
+onready var damageable := true
+onready var color_red := Color(1, 0, 0, 0.8)
 
 
 func _process(_delta):
@@ -15,10 +19,20 @@ func _process(_delta):
 	else:
 		on_destroyed()
 
+
 func on_destroyed():
 		animated_sprite.play("destroyed")
 		collision.disabled = true
 		collision.visible = false
 
 func on_damage(value: int) -> void:
-	service.update_hp(service.get_hp() - value)
+	if damageable:
+		damageable = false
+		hit_timer.start()
+		animated_sprite.self_modulate = color_red
+		service.update_hp(service.get_hp() - value)
+
+
+func _on_HitTimer_timeout():
+	damageable = true
+	animated_sprite.self_modulate = Color(1, 1, 1, 1)
