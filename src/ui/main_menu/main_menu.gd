70aae729169027936e52c72c77b9a63aea5277d1
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-11-03 20:23:29 +0800

    - made stage 1 as the first stage - made player scene as the last stage before final stage

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_NewGameButton_pressed():
-	get_tree().change_scene("res://scene/player/player_scene.tscn")
+	get_tree().change_scene("res://scene/stage/normal_stage/stage_1.tscn")
 
 func _on_OptionButton_pressed():
 	get_tree().change_scene("res://scene/main_menu/option_scene.tscn")
