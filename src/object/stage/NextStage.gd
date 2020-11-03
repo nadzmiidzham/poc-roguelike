b421899e0a763af1b73d1d61f1a2629b2acf2dfd
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-11-03 14:54:58 +0800

    added popup to gate

--- src/object/stage/NextStage.gd
+++ src/object/stage/NextStage.gd
@@ -3,10 +3,13 @@ extends Area2D
 
 export (Array, String) var stage_list
 
+onready var popup := $InteractPopup
+
 onready var interactable := false
 
 
 func _process(_delta):
+	popup.visible = interactable
 	if interactable && Input.is_action_just_pressed("interact"):
 		var next_stage = stage_list[int(rand_range(0, stage_list.size()))]
 		owner.get_tree().change_scene(next_stage)
