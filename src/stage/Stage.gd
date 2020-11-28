ca5054a04f535a16eda842481cbcb8a482ea0e07
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2020-11-28 21:27:48 +0800

    feat: added final stage

--- src/stage/Stage.gd
+++ src/stage/Stage.gd
@@ -8,6 +8,11 @@ onready var scene_loader := $GameController/SceneLoader
 onready var loading_ui := $UI/LoadingUI
 
 
+func _ready():
+	print("stage_list: ", stage_list)
+	print("final_stage_path: ", final_stage_path)
+
+
 func _on_NormalGate_on_interact():
 	var next_stage_path = stage_list[int(rand_range(0, stage_list.size()))]
 	scene_loader.load_scene(next_stage_path)
